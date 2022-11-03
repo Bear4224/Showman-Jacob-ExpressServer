3ba424f886001f1bb8d32f2220dcd91c3f3d9d3a
Jacob Showman <jacob.showman@mrjaffesclass.com>
2022-11-03 12:16:03 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(function (req, res, next) {
+  console.log('Additional processing is done here');
+  req.timestamp = new Date().toString();
+  next();
+})
+
+app.get('/', (req, res) => {
+    res.append('Content-Type', 'text/html');
+    res.send('<html><head></head><body>'+
+      '<h1>Hello World!</h1>'+
+      '<h3>My server is working!!!</h3>'+
+      '<h5>'+req.timestamp+'</h5></body></html>');
+
+})
+
+//------------------------------------------------------------------------
+//stuff added at end of tutorial
+
+app.get('/bbt/episodes', function (req, res) {
+  res.send('BBT episode list');
+})
+
+//variable n, not restricted to numbers only unfortunately.
+app.get('/bbt/episode/:n', function (req, res) {
+  res.send('BBT episode '+req.params.n);
+})
+
+//------------------------------------------------------------------------
+
+//
+app.get('/bbt', function (req, res) {
+  res.send('Big Bang Theory episode information');
+})
+
+//same as .../:n route, redundant
+//app.get('/bbt/episode/:num', function (req, res) {
+//  res.send('This is the /bbt/episode route and the episode number is '+req.params.num);
+//})
+
+
+//------------------------------------------------------------------------
+//catch-all, that's why it's last. Uses a regular expression
+app.get('*', function (req, res) {
+  res.send('This part runs if no other paths catch it');
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+})
+
